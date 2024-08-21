1847630951fbb788110c933ae1b7ceabb92b721e
Vedant-Hande <148268306+Vedant-Hande@users.noreply.github.com>
2024-08-21 21:43:31 +0530

    Updated arrays

--- arrays/script.js
+++ arrays/script.js
@@ -7,21 +7,21 @@ for (let i = 0; i < a.length; i++) {
 
 console.log("The length of an array is : " + a.length);
 a[2] = 70; // changing an array element
-console.log(a);
+console.table(a);
 console.log(" converting array to_String : " + a.toString());
 console.log(" joining an array byu using join(-) : " + a.join("-"));
 console.log(" push() method in array : " + a.push(90));
-console.log(a);
+console.table(a);
 console.log(" pop() method in Array :" + a.pop());
-console.log(a);
-console.log(" shift() method in Array :" + a.shift());
-console.log(a);
-console.log(" unshift() method in Array :" + a.unshift(80));
-console.log(a);
+console.table(a);
+console.table(" shift() method in Array :" + a.shift());
+console.table(a);
+console.table(" unshift() method in Array :" + a.unshift(80));
+console.table(a);
 let a1 = [11, 22, 33, 44];
-console.log("concat() method in array : " + a.concat(a, a1));
+console.table("concat() method in array : " + a.concat(a, a1));
 
 console.log("splice() in array : " + a.splice(1, 2, 66, 44));
 console.log(a);
-console.log("slice() in array : " + a.slice(2, 3)); //it only return 
+console.table("slice() in array : " + a.slice(2, 3)); //it only return
 console.log(a);
