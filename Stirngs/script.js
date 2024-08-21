766c42d1ef9f80e4bb6a7af98943a5eb1b988ae1
Vedant-Hande <148268306+Vedant-Hande@users.noreply.github.com>
2024-08-21 21:34:26 +0530

    Stirngs Updated

---
+++ Stirngs/script.js
@@ -0,0 +1,16 @@
+let real_name = "vedant hande";
+let s_name = "sarthak hande";
+
+console.log(real_name, "<br>");
+console.log(real_name.indexOf("d"), "<br>");
+console.log(real_name.endsWith("d"), "<br>"); // it reutrn ture or false
+console.log(real_name.repeat(2), "<br>");
+console.log(real_name.slice(2, 6), "<br>");
+console.log(real_name.startsWith("t", 3), "<br>");
+console.log(real_name.substring(4, 7), "<br>");
+console.log(real_name.toLocaleLowerCase(), "<br>");
+console.log(real_name.toUpperCase(), "<br>");
+console.log(real_name.valueOf(), "<br>");
+console.log(real_name.charAt(5), "<br>");
+console.table(real_name.length, "<br>");
+console.table(real_name.replace("hande", "sahil"), "<br>");
