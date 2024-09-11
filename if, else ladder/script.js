3f086e6408e333ffaa3bc87a284102c8dcbd121b
Vedant-Hande <148268306+Vedant-Hande@users.noreply.github.com>
2024-09-11 12:29:58 +0530

    IF ELSE updated

--- if, else ladder/script.js
+++ if, else ladder/script.js
@@ -15,7 +15,7 @@ let num1 = 10;
 let num2 = 20;
 console.log("num1 : " + num1);
 console.log("num 2 :" + num2);
-// Arthimatic operator
+// Arthimatic operators
 console.log("num1 + num2 : " + (num1 + num2));
 console.log("num1 - num2 : " + (num1 - num2));
 console.log("num1 * num2 : " + num1 * num2);
@@ -24,14 +24,16 @@ console.log("num1 ** num2 : " + num1 ** num2);
 console.log("num1++: " + num1++); // The  ans is 10 Beacuse  of the number is alwaraday 10 and when we ++ it it's stable if we excuate the same statements aganin than it update by 1
 console.log("+num1: " + +num1);
 
-//  Assignment Operator
+//  Assignment Operators
 num1 += num2;
+// optional
 /*  num1 -= num2;
     num1 *= num2;
     num1 **= num2; 
     num1 %= num2;
 */
 console.log("num1= num1 + num2 : " + num1);
+console.log(num1);
 
 // Conditional Operator
 // === is used to comp- type and number
